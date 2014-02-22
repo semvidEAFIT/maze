ffac3d883864d95cd29b97f1360918fc41b8f808
halzate93 <halzate93@gmail.com>
2014-02-22 17:33:33 -0500

    Created Component MAnager

---
+++ MazeProject/Assets/Multiplayer/MultiplayerComponentManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiplayerComponentManager : MonoBehaviour {
+
+    public MonoBehaviour[] behaviours;
+    
+    void Awake() { 
+        if(networkView.isMine){
+            foreach (MonoBehaviour m in behaviours)
+            {
+                m.enabled = false;
+            }
+        }
+    }
+}
