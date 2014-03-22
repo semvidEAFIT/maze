88e77f5153fda658a1eb0ef149b7562e84fc1ffe
MateoC <carvajalmunoz@gmail.com>
2014-03-22 17:40:37 -0500

    maze generator

---
+++ MazeProject/Assets/Maze/MazeGenerator.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MazeGenerator : MonoBehaviour {
+	/// <summary>
+	/// The floor.
+	/// </summary>
+	public GameObject floor;
+	/// <summary>
+	/// The wall.
+	/// </summary>
+	public GameObject wall;
+	/// <summary>
+	/// The wall materials.
+	/// </summary>
+	public Material[] wallMaterials;
+	/// <summary>
+	/// The size of the maze in x.
+	/// </summary>
+	public int x;
+	/// <summary>
+	/// The size of the maze in y.
+	/// </summary>
+	public int y;
+	private string[,] maze;
+
+	void Start () {
+		maze = new string[x,y];
+		for(int i = 0; i < x;i++){
+			for (int j = 0; j < y; j++){
+				maze[i,j] = "n";
+			}
+		}
+		maze[0,0] = "p";
+		System.Random r = new System.Random();
+		Generate(Vector2.zero,new Vector2(0,-1));
+		for(int i = 0; i < x;i++){
+			for (int j = 0; j < y; j++){
+				if(maze[i,j] == "p"){
+					GameObject.Instantiate(floor,new Vector3(i*2,0,j*2),floor.transform.rotation);
+				}else if(maze[i,j] == "w"){
+					GameObject go = (GameObject)Instantiate(wall,new Vector3(i*2,0,j*2),floor.transform.rotation);
+					int index = r.Next(wallMaterials.Length);
+					go.renderer.material = wallMaterials[index];
+				}
+			}
+		}
+		
+	}
+
+	/// <summary>
+	/// Check neighbors
+	/// </summary>
+	/// <returns>The neighbors.</returns>
+	/// <param name="i">The position in x of the node to check.</param>
+	/// <param name="j">The position in y of the node to check.</param>
+	/// <param name="count">Set this variable to the number of available neighbors.</param>
+	public List <Vector2> CheckNeighbors(int i, int j, out int count){
+		List <Vector2> positions = new List<Vector2>();
+		int max = 0;
+		count = 0;
+		if(i>0 && (maze[i-1,j] == "n"||maze[i-1,j] == "w")){
+			count++;
+			positions.Add(new Vector2(i-1,j));
+		}
+		if(i<x-1 && (maze[i+1,j] == "n"||maze[i+1,j] == "w")){
+			count++;
+			positions.Add(new Vector2(i+1,j));
+		}
+		if(j>0 && (maze[i,j-1] == "n"||maze[i,j-1] == "w")){
+			count++;
+			positions.Add(new Vector2(i,j-1));
+		}
+		if(j<y-1 && (maze[i,j+1] == "n"||maze[i,j+1] == "w")){
+			count++;
+			positions.Add(new Vector2(i,j+1));
+		}
+		return positions;
+	}
+
+	/// <summary>
+	/// Generate the specified p and prev.
+	/// </summary>
+	/// <param name="p">The actual node.</param>
+	/// <param name="prev">Previous node.</param>
+	public void Generate(Vector2 p, Vector2 prev){
+		int count = 0;
+		List<Vector2> nexts = CheckNeighbors((int)p.x,(int)p.y,out count);
+		bool a = (p.x==0 && p.y==0)||(p.x==x-1 && p.y==y-1);
+		bool b =(p.x==0 || p.y==0 || p.x==x-1 || p.y==y-1);
+		int pr = 0;
+		if(nexts.Count==1){
+			System.Random random = new System.Random();
+			pr = random.Next(2);
+		}
+		if((nexts.Count <3 && (!b)) || nexts.Count==1 /*|| nexts.Count==0 */|| pr == 1){
+			maze[(int)p.x,(int)p.y] = "w";
+		}else{
+			maze[(int)p.x,(int)p.y] = "p";
+			int times = 0;
+			while(nexts.Count != 0 /*&& times <4*/){
+				int index = Random.Range(0,nexts.Count);
+				if(index>nexts.Count-1){
+					index= index%nexts.Count;
+				}
+				Vector2 next=nexts[index];
+				if(maze[(int)next.x,(int)next.y]=="n"){
+					Generate(next,p);
+				}
+				nexts.Remove(next);
+			}
+		}
+	}
+	
+}
