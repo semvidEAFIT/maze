8c59cd5081d43fbc03cba3be0012832634e13fa8
luis M Ponce <luisponcekun@gmail.com>
2014-04-05 17:32:53 -0500

    Added the need to rest to Sprint

--- MazeProject/Assets/human/Human.cs
+++ MazeProject/Assets/human/Human.cs
@@ -19,10 +19,24 @@ public class Human : MonoBehaviour {
 
 	private float defaultSpeed;
 
+	private bool canRun;
+
+	/// <summary>
+	/// The time needed to recover between sprints.
+	/// </summary>
+	public float restTime = 10;
+
+	private float timeRested;
+
 	/// <summary>
 	/// The sprint speed.
 	/// </summary>
-	public float sprintSpeed = 20;
+	public float sprintSpeed = 10;
+
+	/// <summary>
+	/// The regen rate.
+	/// </summary>
+	public float regenRate = 1.5f;
 
 	//the one in charge of the character movement
 	private CharacterMotor motor;
@@ -31,36 +45,78 @@ public class Human : MonoBehaviour {
 		if(stepSound == null){
 			Debug.LogError("StepSoundPlayer: step sound not asigned to object " + gameObject.name);
 		}
-		sprintTime/=10;
 		sprintTimeLeft = sprintTime;
 		motor = GetComponent<CharacterMotor>();
 		defaultSpeed = motor.movement.maxForwardSpeed;
-
+		canRun = true;
+		timeRested = 0;
 	}
 
 	void Update () {
-		if((Input.GetAxis("Horizontal")!=0 || Input.GetAxis("Vertical")!=0) && !audio.isPlaying){
-			PlaySound(stepSound);
+//		if((Input.GetAxis("Horizontal")!=0 || Input.GetAxis("Vertical")!=0) && !audio.isPlaying){
+//			PlayStep();
+//		}
+
+
+		if(canRun){
+			if(Input.GetAxis("sprint") > 0){
+				motor.movement.maxForwardSpeed = sprintSpeed;
+
+				if(sprintTimeLeft <= 0){
+					canRun = false;
+					timeRested = 0;
+				} else {
+					sprintTimeLeft -= Time.deltaTime;
+				}
+			}
+			else{
+				motor.movement.maxForwardSpeed = defaultSpeed;
+				if(sprintTimeLeft < sprintTime){
+					sprintTimeLeft += Time.deltaTime * regenRate;
+				}
+			}
 		}
-
-
-		if(Input.GetAxis("sprint")!=0 && sprintTimeLeft>0){
-			motor.movement.maxForwardSpeed =  sprintSpeed;
-			sprintTimeLeft-=Time.deltaTime;
-		} else {
+		else{
 			motor.movement.maxForwardSpeed = defaultSpeed;
-			sprintTimeLeft+=Time.deltaTime;
+			timeRested += Time.deltaTime * regenRate;
+			if(timeRested >= restTime){
+				canRun = true;
+				timeRested = 0;
+				sprintTimeLeft = sprintTime;
+			}
 		}
+
+
+		
+		
+		//		if(sprintTimeLeft <= 0) {
+//			canRun = false;
+//			motor.movement.maxForwardSpeed = defaultSpeed;
+//			timeRested = 0;
+//		}
+//
+//		if(timeRested >= restTime){
+//			canRun = true;
+//		}
+//
+//		if(Input.GetAxis("sprint") != 0 && sprintTimeLeft > 0 && canRun){
+//			motor.movement.maxForwardSpeed =  sprintSpeed;
+//			sprintTimeLeft-=Time.deltaTime;
+//
+//			Debug.Log("runnnnnnnnn");
+//		}
+//		if(sprintTimeLeft < sprintTime && !canRun){
+//			sprintTimeLeft += Time.deltaTime;
+//		}
+//
+//		if(timeRested <= restTime){
+//			timeRested += Time.deltaTime;
+//		}
+//
+		Debug.Log(sprintTimeLeft);
 	}
 
-	/// <summary>
-	/// Plays the sound given as parameter.
-	/// </summary>
-	/// <param name="sound">Sound to play.</param>
-	public void PlaySound(AudioClip sound){
-		audio.clip = sound;
-		audio.Play();
+	public void PlayStep(){
+		audio.PlayOneShot(stepSound);
 	}
-
-
 }
