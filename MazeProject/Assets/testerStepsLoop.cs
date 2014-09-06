78e0a2b9b180bc0742554838dd9df7217ecac20b
luis M Ponce <luisponcekun@gmail.com>
2014-09-06 17:55:37 -0500

    tested monster step sounds

---
+++ MazeProject/Assets/testerStepsLoop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testerStepsLoop : MonoBehaviour {
+
+	public AudioClip[] stepSounds;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!audio.isPlaying)
+		{
+			audio.clip = stepSounds[Random.Range(0, stepSounds.Length-1)];
+			audio.Play();
+		}
+	}
+}
